2800a48f665479e55346a08d0b869f56525584f0
yac0w <105124528+yac0w@users.noreply.github.com>
2023-04-06 04:49:26 +0000

    agregue tablas

--- Relacional.sql
+++ Relacional.sql
@@ -3,3 +3,9 @@ Create table lista(
     PRIMARY KEY (Idtabla)
 );
 
+Create table personas(
+    dni int,
+    nombre varchar(30),
+    PRIMARY KEY (dni),
+    FOREIGN KEY (nombre) references registro(nombre)
+);
